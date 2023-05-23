38d3a0a343bf8f6226b740f2b532c0049142810d
Anurag Pradhan <anuragp936@gmail.com>
2023-05-23 21:43:26 +0530

    Fix Image component issue

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from 'next/future/image';
 
 const Intro = () => {
   return (
     <div className="grid grid-cols-[max-content_auto] gap-3 max-w-2xl px-8 mx-auto ">
       <Image
-        className=" object-contain rounded-lg max-h-full"
+        className="rounded-lg self-center"
         src={'/images/anurag.jpg'}
         width={150}
         height={150}
+        alt="Anuarg Pradhan pic"
       />
       <div className="self-end">
         <h1 className="m-0 font-semibold text-2xl">Anuarg Pradhan</h1>
